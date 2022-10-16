a26127c7e0116a2edb034086db947f9ee0fd89ee
Angel Bulnes <angel_bulnes16@yahoo.com>
2022-10-16 12:14:11 -0600

    Create initial structure

---
+++ src/navigation/auth/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const AuthNavigator = () => {
+  return (
+    <View>
+      <Text>AuthNavigator</Text>
+    </View>
+  );
+};
+
+export default AuthNavigator;
---
+++ src/screens/index.ts
@@ -0,0 +1,3 @@
+export { default as HomeScreen } from './home';
+export { default as NativeLoginScreen } from './native-login';
+export { default as WelcomeScreen } from './welcome';
